92992a4f01243b26c2296bbd3fe0a22847245ce4
geeksploit <max@geeksploit.me>
2018-10-14 01:14:03 +0300

    Attach countdown timer component as an attribute

--- 01_03_countdown_timer/Assets/_Scripts/DigitalCountdown.cs
+++ 01_03_countdown_timer/Assets/_Scripts/DigitalCountdown.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent (typeof (CountdownTimer))]
 public class DigitalCountdown : MonoBehaviour {
 	private Text textClock;
 	private CountdownTimer countdownTimer;
