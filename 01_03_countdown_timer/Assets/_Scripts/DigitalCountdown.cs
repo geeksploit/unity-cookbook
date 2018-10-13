22037803f590cbd387ddd53fe1e2e17389ef2817
geeksploit <max@geeksploit.me>
2018-10-14 00:56:44 +0300

    Add and apply digital countdown script

---
+++ 01_03_countdown_timer/Assets/_Scripts/DigitalCountdown.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DigitalCountdown : MonoBehaviour {
+	private Text textClock;
+	private CountdownTimer countdownTimer;
+
+	void Awake () {
+		textClock = GetComponent<Text>();
+		countdownTimer = GetComponent<CountdownTimer>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		countdownTimer.ResetTimer(30);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int timeRemaining = countdownTimer.GetSecondsRemaining();
+		string message = TimerMessage(timeRemaining);
+		textClock.text = message;
+	}
+
+	private string TimerMessage(int secondsLeft) {
+		if (secondsLeft <= 0)
+			return "Countdown timer\nhas finished";
+		else
+			return "Countdown timer\n" + secondsLeft;
+	}
+}
