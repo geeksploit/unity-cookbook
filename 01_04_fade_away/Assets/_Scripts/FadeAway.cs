3606787484e62932c84725e64b80396e59ed13c9
geeksploit <max@geeksploit.me>
2018-10-14 17:42:19 +0300

    Create a fade away script

---
+++ 01_04_fade_away/Assets/_Scripts/FadeAway.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof (CountdownTimer))]
+public class FadeAway : MonoBehaviour {
+	private CountdownTimer countdownTimer;
+	private Text textUi;
+
+	void Awake () {
+		textUi = GetComponent<Text>();
+		countdownTimer = GetComponent<CountdownTimer>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		countdownTimer.ResetTimer(5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float alphaRemaining = countdownTimer.GetProportionTimeRemaining();
+		print(alphaRemaining);
+		Color newColor = textUi.color;
+		newColor.a = alphaRemaining;
+		textUi.color = newColor;
+	}
+}
